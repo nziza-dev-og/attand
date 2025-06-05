d047b2dd55c1ad3039db2bd8db22c2ceb7faf3cc
Nziza Jules <nzizajules250@gmail.com>
2025-06-05 17:03:48 +0000

    Error: Invalid src prop (https://i.pinimg.com/736x/24/3d/a7/243da749e106

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
@@ -13,6 +14,24 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co', // Keep existing if still needed
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com', // Specifically allow i.pinimg.com
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https', // Or 'http' if you need that too, but HTTPS is safer
+        hostname: '**', // This allows all hostnames
         port: '',
         pathname: '/**',
       },
